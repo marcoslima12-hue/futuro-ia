b598b889741f2eed89ed1dfc811f3a0a08c0c5c1
marcoslima12-hue <marcos.lima12@escola.pr.gov.br>
2026-09-21 08:16:46 -0300

    marcos

---
+++ script.js
@@ -0,0 +1,23 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultados = document.querySelector(".caixa-resultados");
+const textoresultados = document.querySelector(".texto-resultados");
+
+const perguntas = [
+    {
+        enunciado: "pergunta 1"
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+              enunciado: "pergunta 1"
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]       
+    }
+];
+
